6a720f704c463642eb96653d7a88f77474259544
anpavlov11 <apavlo11@gmail.com>
2022-06-02 09:24:51 -0500

    updated view function to use template

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 class Home(View):
     def get(self, request):
         return HttpResponse('Truck Collector Home')
+
+class Home(TemplateView):
+    template_name = 'home.html'
 
 class About(View):
     def get(self, request):
