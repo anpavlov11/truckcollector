bee6038a20357f96d7c05c94ca71c60c94813bdc
anpavlov11 <apavlo11@gmail.com>
2022-06-06 00:17:24 -0500

    created all templates

--- main_app/views.py
+++ main_app/views.py
@@ -7,34 +7,34 @@ from .models import Truck
 
 import truckcollector_project
 
-class Home(View):
-    def get(truck, request):
-        return HttpResponse('Truck Collector Home')
+# class Home(View):
+#     def get(self, request):
+#         return HttpResponse('Home')
 
 class Home(TemplateView):
     template_name = 'home.html'
 
-class About(View):
-    def get(truck, request):
-        return HttpResponse('Truck Collector About')
+# class About(View):
+#     def get(self, request):
+#         return HttpResponse('About')
 
 class About(TemplateView):
     template_name = 'about.html'
 
-# class TruckList(View):
-#     def __init__(truck, name, coolStuff, image):
-#         truck.name = name
-#         truck.coolStuff = coolStuff
-#         truck.image = image
+class Truck:
+    def __init__(self, name, coolStuff, image):
+        self.name = name
+        self.coolStuff = coolStuff
+        self.image = image
         
-#         Truck = [
-#             Truck('2019', 'Ram', '1500', 'I drive a 2019 Ram 1500. My truck has a name--Stevie like Stevie Nicks. My truck has also be moderatley modified. I have done all the aftermarket modifications myself.', 'https://i.imgur.com/wYZiK58.jpg')
-#         ]
+        trucks = [
+            Truck('2019', 'Ram', '1500', 'I drive a 2019 Ram 1500. My truck has a name--Stevie like Stevie Nicks. My truck has also be moderatley modified. I have done all the aftermarket modifications myself.', 'https://i.imgur.com/wYZiK58.jpg'),
+        ]
 
 class TruckList(TemplateView):
     template_name = 'truck_list.html'
 
-    def get_context_data(truck, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['trucks'] = Truck.objects.all()
+        context['trucks'] = Truck
         return context
