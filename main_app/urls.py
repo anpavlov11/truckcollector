a36e693b72a6f80ebbeeb0fd764df7ee381b43b7
anpavlov11 <apavlo11@gmail.com>
2022-06-06 01:16:43 -0500

    updates templates and urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('about/', views.About.as_view(), name='about'),
-    path('trucks/', views.TruckList.as_view(), name='truck_list')
+    path('trucks/', views.TruckList.as_view(), name='truck_list'),
+    path('trucks/new/', views.TruckCreate.as_view(), name="truck_create"),
+    path('trucks/<int:pk>/', views.TruckDetail.as_view(), name="truck_detail"),
+    path('trucks/<int:pk>/update',views.TruckUpdate.as_view(), name="truck_update"),
+    path('trucks/<int:pk>/delete',views.TruckDelete.as_view(), name="truck_delete"),
 ]
